669bd03339444f88431072e837eb0cab74f11e60
Pichsereyvattana Chan <pchan@ithaca.edu>
2022-03-31 17:28:51 -0400

    Created Book, Library, Patron, Librarian, HumanLibrarian, and Kiosk classes. Added stub methods to Librarian, HumanLibrarian, and Kiosk classes.

---
+++ src/main/java/edu/ithaca/dturnbull/bank/Library.java
@@ -0,0 +1,26 @@
+package edu.ithaca.dturnbull.bank;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Library {
+    private static List<Librarian> librarians;
+    private static List<Patron> patrons; 
+    private static List<Book> books;
+    private static int nextLibrarianId = 0;
+    private static int nextPatronId = 0;
+
+    public Library() {
+        librarians = new ArrayList<>();
+        patrons = new ArrayList<>();
+        books = new ArrayList<>();
+    }
+
+    public void increaseNextLibrarianId() {
+        nextLibrarianId++;
+    }
+
+    public int getNextLibrarianId() {
+        return nextLibrarianId;
+    }
+}
---
+++ src/test/java/edu/ithaca/dturnbull/bank/LibrarianTest.java
@@ -0,0 +1,17 @@
+package edu.ithaca.dturnbull.bank;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LibrarianTest {
+    
+    @Test
+    void humanLibrarianConstructorTest() {
+
+    }
+
+    @Test
+    void kioskConstructorTest() {
+        
+    }
+}
