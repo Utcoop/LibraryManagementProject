a119aa9de79ab2434be68a8ba54ec67d23f6c1cf
Jackson <jacksonbckr13@gmail.com>
2022-04-24 16:53:26 -0400

    Wrote methods to add books to wishlist. Wrote tests for addtoWishlist

--- src/main/java/edu/ithaca/dturnbull/LibraryManagementSystem/Patron.java
+++ src/main/java/edu/ithaca/dturnbull/LibraryManagementSystem/Patron.java
@@ -11,11 +11,12 @@ public class Patron {
     List<Book> booksOut;
     int maxBooks = 5;
     List<Book> wishlist;
+    Library lib;
 
-
-    public Patron(Library library, String name, String password){
-        Id = library.getNextPatronId();
-        library.increaseNextPatronId();
+    public Patron(Library library, String name, String password) {
+        lib = library;
+        Id = lib.getNextPatronId();
+        lib.increaseNextPatronId();
         this.password = password;
         this.name = name;
         fines = 0.0;
@@ -23,36 +24,57 @@ public class Patron {
         wishlist = new LinkedList<>();
     }
 
-
     /**
      * 
      * @param book book to add to wishlist
+     * @throws InvalidBookException
+     * @return True if the book was added from wishlist successfully and false
+     *         otherwise
      */
-    public void addToWishlist( Book book){
-        //TODO
+    public boolean addToWishlist(String bookT) throws InvalidBookException {
+        List<Book> books = lib.getBooks();
+        for (int i = 0; i < books.size(); i++) {
+            if (books.get(i).getTitle().equals(bookT)) {
+                if (!wishlist.contains(books.get(i))) {
+                    wishlist.add(books.get(i));
+                    return true;
+                } else {
+                    throw new InvalidBookException("This book is already in your wishlist");
+                }
+            }
+        }
+        throw new InvalidBookException("We do not have this book");
     }
 
     /**
-     * 
      * @param book book to remove from wishlist
+     * @throws InvalidBookException
+     * @return True if the book was removed from wishlist successfully and false
+     *         otherwise
      */
-    public void removeFromWishlist(Book book){
-        //TODO
+    public boolean removeFromWishlist(String bookT) throws InvalidBookException {
+        for (int i = 0; i < wishlist.size(); i++) {
+            if (wishlist.get(i).getTitle().equals(bookT)) {
+                wishlist.remove(i);
+                return true;
+            }
+        }
+        throw new InvalidBookException("This book doesn't exist in the library");
     }
 
     /**
-     * @return the fines that  are outstanding in this account
+     * @return the fines that are outstanding in this account
      */
-    public double checkFines(){
-        //TODO
+    public double checkFines() {
+        // TODO
         return -1;
     }
 
     /**
      * @return a string that represents the current wishlist
      */
-    public String checkWishlist(){
-        //TODO
+    public String checkWishlist() {
+        // TODO
         return "";
     }
 
@@ -61,20 +83,20 @@ public class Patron {
      * @param book
      * @return True if the book was checked out successfully and false otherwise
      */
-    public Boolean bookCheckout(Book book){
-        //TODO
+    public Boolean bookCheckout(Book book) {
+        // TODO
         return false;
     }
 
-    public int getId(){
+    public int getId() {
         return Id;
     }
 
-    public String getName(){
+    public String getName() {
         return name;
     }
 
-    public String getPassword(){
+    public String getPassword() {
         return password;
     }
 }
--- src/test/java/edu/ithaca/dturnbull/LibraryManagementSystem/PatronTest.java
+++ src/test/java/edu/ithaca/dturnbull/LibraryManagementSystem/PatronTest.java
@@ -28,5 +28,30 @@ public class PatronTest {
         assertEquals("password", vattanaPatron.getPassword());
     }
 
+    @Test
+    void wishlistAddTest() throws Exception{
+        Library lib = new Library();
+        Patron jackson = new Patron(lib, "Jackson", "password");
+        lib.addBook("Percy Jackson", "Rick Riordan", "1234567891012", "02-12-2000", "Adventure", 10.00, 1);
+        lib.addBook("Percy Jackson 2", "Rick Riordan", "1234567891012", "02-12-2000", "Adventure", 10.00, 1);
+        lib.addBook("Percy Jackson 3", "Rick Riordan", "1234567891012", "02-12-2000", "Adventure", 10.00, 1);
+        lib.addBook("Percy Jackson 4", "Rick Riordan", "1234567891012", "02-12-2000", "Adventure", 10.00, 1);
+        lib.addBook("Percy Jackson 5", "Rick Riordan", "1234567891012", "02-12-2000", "Adventure", 10.00, 1);
+        lib.addBook("Coding for Dumbies", "Some Guy", "1234567891012", "02-12-2000", "Adventure", 10.00, 1);
+        
+        //Ensures that all the example books can be added to a wishlist successfully
+        assertTrue(jackson.addToWishlist("Percy Jackson"));
+        assertTrue(jackson.addToWishlist("Percy Jackson 2"));
+        assertTrue(jackson.addToWishlist("Percy Jackson 3"));
+        assertTrue(jackson.addToWishlist("Percy Jackson 4"));
+        assertTrue(jackson.addToWishlist("Percy Jackson 5"));
+        assertTrue(jackson.addToWishlist("Coding for Dumbies"));
 
+        //Tests to make sure exceptions are thrown if the book title is not found in the library.
+        assertThrows(InvalidBookException.class,()->jackson.addToWishlist("Perry Jackson"));
+        assertThrows(InvalidBookException.class,()->jackson.addToWishlist("ASDF"));
+        //Tests to make sure the same book can't be added to the library at the same time.
+        assertThrows(InvalidBookException.class,()->jackson.addToWishlist("Percy Jackson"));
+
+    }
 }
