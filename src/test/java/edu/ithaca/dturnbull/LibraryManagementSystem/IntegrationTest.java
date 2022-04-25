4af063b8d159232193fadda8edfa10bc7997a3fa
Pichsereyvattana Chan <pchan@ithaca.edu>
2022-04-25 13:11:23 -0400

    added comments

--- src/test/java/edu/ithaca/dturnbull/LibraryManagementSystem/IntegrationTest.java
+++ src/test/java/edu/ithaca/dturnbull/LibraryManagementSystem/IntegrationTest.java
@@ -16,32 +16,32 @@ public class IntegrationTest {
 
     @Test
     void IntegrationTest() {
-        LibrarianTest(mainLibrary,librarianList);
-        LibraryTest(mainLibrary,librarianList);
-        PatronTest(mainLibrary,librarianList);
+        LibrarianTest(mainLibrary,librarianList); //test for Librarian Class
+        LibraryTest(mainLibrary,librarianList); //test for Library Class
+        PatronTest(mainLibrary,librarianList); //test for Patron Class
     }
 
     void LibrarianTest(Library mainLibrary, List<Librarian> librarianList) {
-        HumanLibrarian librarian1 = new HumanLibrarian(mainLibrary, "Willson Tybur", "123");
+        HumanLibrarian librarian1 = new HumanLibrarian(mainLibrary, "Willson Tybur", "123"); //initialize a HumanLibrarian object
         assertEquals("Willson Tybur", librarian1.getName());
         assertEquals(0, librarian1.getId());
 
-        HumanLibrarian librarian2 = new HumanLibrarian(mainLibrary, "Kenny S", "321");
+        HumanLibrarian librarian2 = new HumanLibrarian(mainLibrary, "Kenny S", "321"); //initialize a HumanLibrarian object
         assertEquals("Kenny S", librarian2.getName());
         assertEquals(1, librarian2.getId());
 
-        HumanLibrarian librarian3 = new HumanLibrarian(mainLibrary, "S1mple", "09890");
+        HumanLibrarian librarian3 = new HumanLibrarian(mainLibrary, "S1mple", "09890"); //initialize a HumanLibrarian object
         assertEquals("S1mple", librarian3.getName());
         assertEquals(2, librarian3.getId());
 
         Kiosk kiosk1 = new Kiosk(mainLibrary);
-        assertEquals(3, kiosk1.getId());
+        assertEquals(3, kiosk1.getId()); //initialize a Kiosk object
 
         Kiosk kiosk2 = new Kiosk(mainLibrary);
-        assertEquals(4, kiosk2.getId());
+        assertEquals(4, kiosk2.getId()); //initialize a Kiosk object
 
         Kiosk kiosk3 = new Kiosk(mainLibrary);
-        assertEquals(5, kiosk3.getId());
+        assertEquals(5, kiosk3.getId()); //initialize a Kiosk object
 
 
         librarianList.add(librarian1);
@@ -52,30 +52,32 @@ public class IntegrationTest {
         librarianList.add(kiosk3);
         assertEquals(6, librarianList.size());
 
-        librarian1.addPatron("Vattana", "123");
+        //create 5 patrons using librarian1 instance
+        librarian1.addPatron("Vattana", "123"); 
         librarian1.addPatron("Jackson", "321");
         librarian1.addPatron("VZ", "0909");
         librarian1.addPatron("Yelena", "007");
         librarian1.addPatron("Katy", "555");
-
-        assertEquals(6, librarianList.size());
     }
 
   
     void LibraryTest(Library mainLibrary, List<Librarian> librarianList) {
+        //add librarians to the mainLibrary object
         for (int i = 0; i < librarianList.size(); i++) {
-            mainLibrary.addLibrarian(librarianList.get(i)); 
+            mainLibrary.addLibrarian(librarianList.get(i));  
         }
 
-        assertEquals(6, librarianList.size());
-        assertEquals(6, mainLibrary.getLibrarians().size());
+        //check if librarians size in the mainLibrary is the same as librarianList size
+        assertEquals(librarianList.size(), mainLibrary.getLibrarians().size());
 
+        //check if both lists are pointing to the same objects
         for (int j = 0; j < mainLibrary.getLibrarians().size(); j++) {
             assertEquals(librarianList.get(j), mainLibrary.getLibrarians().get(j));
         }
     }
 
     void PatronTest(Library mainLibrary, List<Librarian> librarianList) {
+        //check if patrons added by HumanLibrarian objects are in the mainLibrary's patron list with correct information
         assertEquals("Vattana", mainLibrary.getPatrons().get(0).getName());
         assertEquals(0, mainLibrary.getPatrons().get(0).getId());
 
